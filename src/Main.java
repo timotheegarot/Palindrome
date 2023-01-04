2a3c184fda224a61d2eedbdeecdac53ff105051d
timotheegarot <timotheegarot@hotmail.com>
2023-01-04 14:44:57 +0100

    Create Repository

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Saisissez un mot : ");
+        String word = scanner.nextLine();
+        char[] letters;
+        String word_Reverse;
+
+        for (int i; i < letters.length; i++) {
+
+        }
+
+        System.out.println("Le mot " + word + " à l'envers donne " + word_Reverse);
+    }
+}
