be79c257dda4fbb85cf534485b2dc74858aa7ca3
timotheegarot <timotheegarot@hotmail.com>
2023-02-26 13:26:11 +0100

    First test

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,13 +1,19 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import junit.framework.TestCase;
+
+
 public class MainTest {
 
-    public void MirrorWord() {
+    @Test
+    public void testMirrorWord() throws Exception {
         /*QUAND on saisit une chaîne ALORS celle-ci est renvoyée en miroir*/
         //Arrange
+        String testWord = "test";
         //Act
         //Assert
+        Assert.assertEquals("tset", Main.reverse(testWord));
     }
     
     // QUAND on saisit un palindrome ALORS celui-ci est renvoyé ET « Bien dit » est envoyé ensuite
